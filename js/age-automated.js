2f614c9e4dcbf5507ef27bc55160e7ca093d93ce
GitMichaGit <mbeyer98@yahoo.com>
2023-11-15 12:07:47 +0100

    added content, todo, fixed age/text automation

--- js/age-automated.js
+++ js/age-automated.js
@@ -7,7 +7,7 @@ function updateText() {
   const ageSeconds = Math.floor(ageMilliseconds / 1000);
   const years = (ageSeconds / (365.25 * 24 * 60 * 60)).toFixed(9);
 
-  const newText = `Hi there! I'm a ${years} year old junior software developer.`;
+  const newText = `Hallöchen! Ich bin ein  ${years} Jahre junger Junior Software Developer, der Spaß an der Entwicklung hat! Unten findet Ihr meine derzeitigen Erfolge`;
 
   textContainer.innerHTML = '';
 
